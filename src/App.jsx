07b2eb118b2f3fb41dc1aa98b34e3747e46923bc
sandesb <bajracharyasandeshh@gmail.com>
2024-09-05 17:00:24 +0545

    root help popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { toggleHelpPopup } from "./redux/uiActions"; // Import the action
 
 import BodyLayout from './theme/BodyLayout';
 import MyClasses from './pages/MyClasses';
@@ -9,27 +10,40 @@ import Messges from './pages/Messges';
 import HelpCenter from './pages/HelpCenter';
 import HomePage from './pages/HomePage';
 import Notes from './pages/Notes'; // Import the Notes component
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Check if the popup has already been shown
+    const isHelpPopupShown = localStorage.getItem('helpPopupShown');
+
+    if (!isHelpPopupShown) {
+      // Show the popup and set a flag in localStorage to prevent future popups
+      const timer = setTimeout(() => {
+        dispatch(toggleHelpPopup());
+        localStorage.setItem('helpPopupShown', 'true'); // Set flag to true
+      }, 1200);  // 2-second delay
+
+      return () => clearTimeout(timer);  // Cleanup the timeout
+    }
+  }, [dispatch]);  // Only run once when the app first loads
+
   return (
-    <Provider store={store}>
+    <>
       <Toaster position="bottom-center" />
-      <Router>
-        <Routes>
-          <Route path="/" element={<BodyLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="my-classes" element={<MyClasses />} />
-            <Route path="account" element={<Account />} />
-            <Route path="messages" element={<Messges />} />
-            <Route path="help-center" element={<HelpCenter />} />
-            <Route path="notes/:id" element={<Notes />} /> {/* Add this route */}
-          </Route>
-        </Routes>
-      </Router>
-    </Provider>
+      <Routes>
+        <Route path="/" element={<BodyLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="my-classes" element={<MyClasses />} />
+          <Route path="account" element={<Account />} />
+          <Route path="messages" element={<Messges />} />
+          <Route path="help-center" element={<HelpCenter />} />
+          <Route path="notes/:id" element={<Notes />} /> {/* Add this route */}
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import Router
+import { Provider } from 'react-redux'; // Import Provider
+import { store } from './redux/store'; // Import your Redux store
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>  {/* Wrap App with Provider */}
+      <Router>  {/* Wrap App with Router */}
+        <App />
+      </Router>
+    </Provider>
+  </StrictMode>
+);
