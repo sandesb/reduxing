cb042b3f8de8188af9427f85cac7b5138265f406
sandesb <bajracharyasandeshh@gmail.com>
2024-11-15 01:38:43 +0545

    redirects fix

--- src/landing-page/Main.jsx
+++ src/landing-page/Main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import { useEffect } from "react";
 const Main = () => {
 	return (
 		<main>
